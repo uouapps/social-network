09d188ed7c9c5ba5ab6998b45abbdd5683b0e8c1
adnanarf <adnanarf@gmail.com>
2016-04-11 04:01:46 +0500

    Updates Slider Script

--- social-network/assets/js/scripts.js
+++ social-network/assets/js/scripts.js
@@ -15,6 +15,20 @@ var contactWindow = $(contact).find('.contact-details');
 $('.sponsors-slider').owlCarousel({
   items: 6
 });
+$('.texti-slide').owlCarousel({
+    loop:true,
+    nav:true,
+	items: 2,
+	navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+}});
+
 
 $(contact).on('click', function(e){
   $(this).find(contactWindow).toggle();
