7dab93c2357bafe12c7bc83d08966da9a67a6da3
parvez210 <parvez08eee@gmail.com>
2016-05-12 12:53:01 +0600

    fixed search button

--- tiger/searchform.php
+++ tiger/searchform.php
@@ -1,5 +1,5 @@
 <form class="search-form"  method="get"   action="<?php echo esc_url( home_url( '/','tiger' ) ); ?>">
 	<input type="text" class="search-input" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" id="s" placeholder="<?php esc_html_e( 'Search', 'tiger' ); ?>"/>
-	<input type="submit" class="btn btn-default searchsubmit"  />
+	<input type="submit" class="btn btn-default searchsubmit" value="&#xf002"/>
 </form>
 
