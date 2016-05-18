18fea8961008bb303c7d2953235300d90058aa8d
parvez210 <parvez08eee@gmail.com>
2016-05-18 18:42:16 +0600

    some change

--- tiger/templates/headerS/header-one.php
+++ tiger/templates/headerS/header-one.php
@@ -2,19 +2,19 @@
       <div class="uou-block-2b">
         <div class="container">
         <?php $tiger_option_data =get_option('tiger_option_data'); ?>
-        <?php 
-        $top_logo_image= tiger_IMAGE."tiger-logo.png";              
-        if(isset($tiger_option_data['tiger-header-icon']['url']) AND $tiger_option_data['tiger-header-icon']['url']!=""):         
-			$top_logo_image=esc_url($tiger_option_data['tiger-header-icon']['url']); 
-         endif; ?> 
+        <?php
+        $top_logo_image= tiger_IMAGE."tiger-logo.png";
+        if(isset($tiger_option_data['tiger-header-icon']['url']) AND $tiger_option_data['tiger-header-icon']['url']!=""):
+			$top_logo_image=esc_url($tiger_option_data['tiger-header-icon']['url']);
+         endif; ?>
          <a href="<?php echo esc_url(site_url('/')); ?>" class="logo"> <img src="<?php echo esc_attr($top_logo_image); ?>" alt="<?php esc_html_e( 'image', 'tiger' ); ?>"> </a>
-          
-        
-                
+
+
+
         <a href="#" class="mobile-sidebar-button mobile-sidebar-toggle"><span></span></a>
 
-          <nav class="nav"> 
-        <?php 
+          <nav class="nav">
+        <?php
 
           $defaults = array(
             'theme_location'  => 'primary_navigation_right',
@@ -24,7 +24,7 @@
             'container_id'    => '',
             'menu_class'      => 'sf-menu',
             'menu_id'         => '',
-            'echo'            => true,            
+            'echo'            => true,
             'before'          => '',
             'after'           => '',
             'link_before'     => '',
@@ -36,20 +36,20 @@
           );
 
           wp_nav_menu( $defaults );
-					
-			
-			?>		
+
+
+			?>
           </nav>
           <?php
-			  if ( !has_nav_menu( 'primary_navigation_right' ) AND !has_nav_menu( 'primary_navigation_left' )  ) {				
-					
+			  if ( !has_nav_menu( 'primary_navigation_right' ) AND !has_nav_menu( 'primary_navigation_left' )  ) {
+
 					 $defaults = array(
 					'theme_location'  => '',
 					//'menu'            => 'primary-menu',
-					'container'       => 'div',
+					'container'       => 'ul',
 					'container_class' => '',
 					'container_id'    => '',
-					'menu_class'      => 'menu',
+					'menu_class'      => 'sf-menu',
 					'menu_id'         => '',
 					'echo'            => true,
 					'fallback_cb'     => 'wp_page_menu',
@@ -63,30 +63,30 @@
 				);
 
 
-				
+
 				?>
-				<nav class="nav"> 
-										
+				<nav class="nav">
+
 				<?php
-				//wp_nav_menu( $defaults );
-				
+				wp_nav_menu( $defaults );
+
 					/*
-					 wp_nav_menu( array( 
+					 wp_nav_menu( array(
 						'theme_location' => 'primary_navigation_right',
-						'container'       => true, 
-						'menu_class'      => 'menu',						
+						'container'       => true,
+						'menu_class'      => 'menu',
 						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-						) ); 
+						) );
 						*/
-						
+
 					?>
 				</nav>
-				<?php	
-							
-					
+				<?php
+
+
 				}
           ?>
-                    
+
         </div>
       </div> <!-- end .uou-block-2b -->
     </div>
